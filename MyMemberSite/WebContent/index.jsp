9dc36eb590a626095e9f82032753d96d379240ec
a01076880150 <a01076880150@gmail.com>
2019-03-15 14:48:31 +0900

    작성자: 조혜선
    내용
    강의 연습용
    
    할일
    오류부터 해결하자
    CRUD 적용

---
+++ MyMemberSite/WebContent/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+ 	<jsp:include page="/Header.jsp"/>
+	<a href='./myMember/listTest'>연습 페이지로 이동</a>
+
+<!-- 	<form action="./member/one" method="get"> -->
+<!-- 		검색할 아이디: <input type="text" name="email" placeholder="이메일을 입력해주세요"> -->
+<!-- 		비밀번호를 입력하세요: <input type="password" name="pwd" -->
+<!-- 			placeholder="비밀번호를 입력해주세요"> <input type="submit" value="검색"> -->
+
+<!-- 	</form> -->
+	<jsp:include page="/Tail.jsp"/>
+</body>   
+</html>        
---
+++ MyMemberSite/src/net/tom/member/MemberAdd.java
@@ -0,0 +1,89 @@
+package net.tom.member;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value="/myMember/add")
+public class MemberAdd extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		res.sendRedirect("./memberForm.jsp");
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		 
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "jsp";
+		String password =" jsp";
+		
+//		req.setCharacterEncoding("UTF-8");
+		
+		String emailStr = req.getParameter("email");
+		String pwdStr = req.getParameter("password"); 
+		String nameStr = req.getParameter("name");
+		
+		String sql = ""; 
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, password);
+			
+			sql = "INSERT INTO MEMBERS";
+			sql += " (MNO, EMAIL, PWD, MNAME, CRE_DATE, MOD_DATE)";
+			sql += " VALUES(MEMBERS_MNO_SEQ.NEXTVAL, ?, ?, ?, SYSDATE, SYSDATE)";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, emailStr);
+			pstmt.setString(2, pwdStr);
+			pstmt.setString(3, nameStr);
+			
+			pstmt.executeUpdate();
+			
+			res.sendRedirect("./listTest");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			req.setAttribute("error", e);
+			RequestDispatcher dispatcher = 
+					req.getRequestDispatcher("/error.jsp");
+			dispatcher.forward(req, res);
+		}finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} // finally end
+	}
+}
